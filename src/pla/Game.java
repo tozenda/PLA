7251305a5de500b1b75eb3be62f23e9015f03383
Ferreira <jojo.ferreira@live.fr>
2017-06-02 23:16:18 +0200

    creation de tous ce qui est graphique, récupérations des événements clavier et souris, REGARDER LES COMMENTAIRES dans le code

--- src/pla/Game.java
+++ src/pla/Game.java
@@ -1,9 +1,106 @@
 package pla;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.Timer;
+
+@SuppressWarnings("unused")
 public class Game {
 	
+	static Game game;	
 	public static void main(String[] args) {
-		
+		game = new Game();
 	}
+	
+	JFrame m_frame;
+	GameView m_view;
+	Timer m_timer;
+	GameModel m_model;
+	GameController m_controller;
+	JLabel m_text;
+	int m_fps;
+	String m_msg;
+	long m_elapsed;
+	long m_lastRepaint;
+	long m_lastTick;
+	int m_nTicks;
+	
+	public Game() {
+		m_model = new GameModel(this);
+	    m_controller = new GameController(this, m_model);
+	    
+	    createWindow();
 
+	    // create the main window and the periodic timer
+	    // to drive the overall clock of the simulation.
+	    
+//	    createTimer();
+	}
+	
+	private void createWindow() {
+		
+		// creation QUE de la fenetre
+	    m_frame = new JFrame();
+	    m_frame.setTitle("Conquerir");
+	    m_frame.setSize(1000, 600);
+	    
+	    // gestion de l'affichage affichage
+	    m_view = new GameView(this, m_model, m_controller);
+	    m_frame.setVisible(true);
+	    
+	    
+	    //permet de fermer la fenetre et arréter l'appli
+	    m_frame.addWindowListener(new WindowListener());
+	  }
+	
+	  private void createTimer() {
+	    int tick = 1; // milliseconds
+	    m_timer = new Timer(tick, new ActionListener() {
+	      public void actionPerformed(ActionEvent evt) {
+	        tick();
+	      }
+	    });
+	    m_timer.start();
+	  }
+	  
+	  static final int REPAINT_DELAY = (int) (1000.0 / 24.0);
+	  
+	  // affichage des ticks de raffraichissement + fps
+	  private void tick() {
+		    long now = System.currentTimeMillis();
+		    m_elapsed += (now - m_lastTick);
+		    m_lastTick = now;
+		    m_nTicks++;
+		    m_model.step(now);
+		    m_controller.step(now);
+		    long elapsed = now - m_lastRepaint;
+		    if (elapsed > REPAINT_DELAY) {
+		      double tick = (double) m_elapsed / (double) m_nTicks;
+		      long tmp = (long) (tick * 10.0);
+		      tick = tmp / 10.0;
+		      m_elapsed = 0;
+		      m_nTicks = 0;
+		      String txt = "Tick=" + tick + "ms";
+		      while (txt.length()<15)
+		        txt += " ";
+		      txt = txt + m_fps + " fps   ";
+		      while (txt.length()<25)
+		        txt += " ";
+		      if (m_msg != null)
+		        txt += m_msg;
+		      m_text.setText(txt);
+		      m_text.repaint();
+		      m_view.repaint();
+		      m_lastRepaint = now;
+		    }
+		  }
+	  
+	  public void setFPS(int fps, String msg) {
+		    m_fps = fps;
+		    m_msg = msg;
+	  }
 }
---
+++ src/pla/GameView.java
@@ -0,0 +1,81 @@
+package pla;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class GameView extends JPanel {
+
+  private static final long serialVersionUID = 1L;
+
+  Game m_game;
+  GameModel m_model;
+  Color m_background = Color.RED;
+  long m_last;
+  int m_npaints;
+  int m_fps;
+  int tailleCase = 40;
+
+  GameView(Game game, GameModel model, GameController ctr) {
+    super(true);
+    m_game = game;
+    m_model = model;
+    
+    // permet de récupérer les événements clavier et souris
+    addKeyListener(ctr);
+    addMouseListener(ctr);
+    addMouseMotionListener(ctr);
+
+    // grab the focus on this JPanel, meaning keyboard events
+    // are coming to our controller. Indeed, the focus controls
+    // which part of the overall GUI receives the keyboard events.
+    setFocusable(true);
+    requestFocusInWindow();
+    grabFocus();
+  }
+  
+  // incrémente une variable à chaque raffraichissement d'écran pour calculer les fps
+  private void computeFPS() {
+    long now = System.currentTimeMillis();
+    if (now - m_last > 1000L) {
+      m_fps = m_npaints;
+      m_last = now;
+      m_npaints = 0;
+    }
+    m_game.setFPS(m_fps, "npaints="+m_npaints);
+    // System.out.println("npaints="+m_npaints);
+    m_npaints++;
+  }
+
+
+  //pour afficher des objets sur notre fenetre
+  public void paintComponent(Graphics g) {
+	  	
+	  	computeFPS();
+    
+	  	//Quadrillage de la map
+  		g.setColor(Color.BLACK);
+  		for(int i = 1; i < 21 ; i++){
+  			g.drawLine(i*tailleCase, 0, i*tailleCase, 480);
+  		}
+  		for(int i = 1; i <= 12 ; i++){
+  			g.drawLine(0, i*tailleCase, 800, i*tailleCase);
+  		}
+  		
+  		//Base des 2 joueurs
+  		g.setColor(Color.RED);
+  		g.fillRect(0, 10*tailleCase, 2*tailleCase, 2*tailleCase);
+  		g.setColor(Color.BLUE);
+    	g.fillRect(18*tailleCase, 0, 2*tailleCase, 2*tailleCase);
+    		
+    	//Quelques obstacles
+    	g.setColor(Color.BLACK);
+   		g.fillRect(4*tailleCase, 3*tailleCase, 1*tailleCase, 3*tailleCase);
+    	g.fillRect(5*tailleCase, 3*tailleCase, 2*tailleCase, 1*tailleCase);
+    	g.fillRect(13*tailleCase, 8*tailleCase, 3*tailleCase, 1*tailleCase);
+   		g.fillRect(15*tailleCase, 6*tailleCase, 1*tailleCase, 2*tailleCase);
+    	g.fillRect(9*tailleCase, 5*tailleCase, 2*tailleCase, 2*tailleCase);
+    		
+  }
+}
